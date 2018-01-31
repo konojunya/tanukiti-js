5c5b27190770394aecd1d42f9475a42d924f0891
tanukiti <33892948+tanukiti@users.noreply.github.com>
2018-01-31 17:09:07 +0900

    2問目とけたお！！ふもっふ！！

--- practice/1/script.js
+++ practice/1/script.js
@@ -3,7 +3,7 @@
 console.log(document.getElementsByTagName('h1')[0].childNodes[0].nodeValue);
 // 2. DOMを変更しよう
 // h1の中のテキストを "Hello from Tanukiti" に変更しよう。
-
+document.querySelector("h1").textContent = "Hello from Tanukiti";
 // 3. DOMを追加しよう
 // h1とpの間にpタグで何か好きな文章を追加しよう。
 
