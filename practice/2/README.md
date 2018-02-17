f5cb4870edb75d5d7e1236c9f4abad423102b55e
konojunya <junya03dance@gmail.com>
2018-02-18 04:24:39 +0900

    2

---
+++ practice/2/README.md
@@ -0,0 +1,4 @@
+# HTTP Requestとは
+
+1. HTTP Requestの発行をしてみよう（jquery ajax）
+2. Ajaxを使って特定のデータを参照しよう（iTunes APIを用いて好きなアーティストのデータを取得する）
---
+++ practice/2/index.js
@@ -0,0 +1,15 @@
+$(function () {
+
+  // itunes apiの使い方
+  // baseURL: https://itunes.apple.com
+  // endpoint: /search
+  // 必要なもの
+  // term: アーティスト名などのキーワード
+  // country: jp
+  // lang: ja_jp
+  // methodはGET
+
+  // 上記の情報を用いてiTunes APIからデータを取得してくる
+  // 取得したものをconsole.logするだけでよい
+
+})
